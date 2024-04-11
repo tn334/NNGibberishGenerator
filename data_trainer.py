1b99db5cb15b4667c25a7081e04492d5494a4efb
Erick Salazar <e.salazar.mexica1@gmail.com>
2024-04-10 18:26:02 -0700

    started frequency tree

--- data_trainer.py
+++ data_trainer.py
@@ -1,29 +1,69 @@
-# start with empty dict
-frequencyDict = { letter: {} for letter in 'abcdefghijklmnopqrstuvwxyz'}
 
-# read all words in txt file
-with open('words_alpha.txt', 'r') as file:
-    words = file.readlines()
+# classes to handle creation of frequency tree
+class TreeNode:
+    def __init__(self) -> None:
+        self.letter = ""
+        self.frequencyCount = 0
+        self.children = dict()
+        self.endOfWord = False
 
-for word in words:
-    word = word.strip()
+class FrequencyTree:
+    def __init__(self) -> None:
+        self.root = TreeNode() # set root of tree
+    
+    def insertWord(self, word):
+        currentNode = self.root # wkg pointer for root
 
-    for letter in word:
-        ...
+        # loop through letters in word
+        for char in word:
 
-# read current word loop
-    # read first word
+            # check if letter is not a child of current node
+            if char not in currentNode.children:
+                currentNode.children[char] = TreeNode() # add child node
+            
+            # move to next letter (node)
+            currentNode = currentNode.children[char]
+        currentNode.endOfWord = True
 
-    # read first letter of word
+    def printTree(self, node=None, word=''):
+        if node is None:
+            node = self.root
+        
+        if node.endOfWord:
+            print(word)
+        
+        for char, childNode in node.children.items():
+            self.printTree(childNode, word + char)
 
-    # 
+
+
+
+def main():
+    # read all words in txt file
+    with open('test.txt', 'r') as file:
+        words = file.readlines()
+
+    tree = FrequencyTree()
+
+    for word in words:
+        word = word.strip()
+        tree.insertWord(word)
+    
+    tree.printTree()
+
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
 
 
-# create freq dict
 
+############################   OLD CODE THAT CAN BE USEFUL   ###################################   
+#frequencyDict = { letter: {} for letter in 'abcdefghijklmnopqrstuvwxyz'}
 
 # # loop through words
 # for word in words:
