868a477be843a2a0a9c6b783f95f212b554b8b2f
Erick Salazar <e.salazar.mexica1@gmail.com>
2024-04-29 17:44:04 -0700

    Added 5 letter dictionary

--- Python/rnn_lstm_model.py
+++ Python/rnn_lstm_model.py
@@ -3,11 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-data = open("../Training Files/words_alpha.txt").readlines()
-data = list(data)
+with open("../Training Files/words_alpha.txt", "r") as input_file:
+    lines = input_file.readlines()
 
 # input_size = # size of input features (26)
 # hidden_size = # number of features in the hidden state (512)
 # num_layers = # number of LSTM layers (8)
 # output_size = # size of the output (7)
 # model = LSTMModel(input_size, hidden_size, num_layers, output_size)
+
+with open("../Output/5_letter_dict.txt", "w") as file_out:
+    for line in lines:
+        words = line.split()
+
+        for word in words:
+            if len(word) <= 5:
+                file_out.write(word + "\n")
